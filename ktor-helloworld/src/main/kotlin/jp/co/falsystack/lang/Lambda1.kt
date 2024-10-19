b14118c80b9d181759649304be3fbeeb1cbd30c5
falsystack <falsystack@gmail.com>
2024-10-19 23:15:50 +0900

    ktor-helloworld: Lambda1

---
+++ ktor-helloworld/src/main/kotlin/jp/co/falsystack/lang/Lambda1.kt
@@ -0,0 +1,14 @@
+package jp.co.falsystack.lang
+
+fun nonLambdaAdd(a: Int, b: Int): Int {
+    return a + b
+}
+
+val lambdaAdd: (Int, Int) -> Int = { a, b -> a + b }
+
+fun main() {
+    val a = 10
+    val b = 5
+    println(nonLambdaAdd(a, b))
+    println(lambdaAdd(a, b))
+}
