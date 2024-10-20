633462fb63007f70f89380657e1710873d8c9e47
falsystack <falsystack@gmail.com>
2024-10-21 00:03:40 +0900

    zcafe-02 start

---
+++ zcafe-02/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import com.example.config.configureDatabase
+import com.example.config.configureRouting
+import com.example.config.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureDatabase()
+    configureSerialization()
+    configureRouting()
+}
